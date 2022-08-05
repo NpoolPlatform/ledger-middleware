d14e6b689f09d77c775987b068f31ae49786e718
Zhao KK <kikakkz@hotmail.com>
2022-08-05 01:37:54 +0000

    Add book keeping and unlock balance

--- api/ledger/create.go
+++ api/ledger/create.go
@@ -3,11 +3,32 @@ package ledger
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
 	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v1/ledger"
 
 	"github.com/NpoolPlatform/ledger-manager/api/detail"
+
+	ledger1 "github.com/NpoolPlatform/ledger-middleware/pkg/ledger"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) BookKeeping(ctx context.Context, in *npool.BookKeepingRequest) (*npool.BookKeepingResponse, error) {
-	if err := detail.
+	if err := detail.Validate(in.GetInfo()); err != nil {
+		logger.Sugar().Errorw("BookKeeping", "error", err)
+		return &npool.BookKeepingResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	if err := ledger1.BookKeeping(ctx, in.GetInfo()); err != nil {
+		logger.Sugar().Errorw("BookKeeping", "error", err)
+		return &npool.BookKeepingResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.BookKeepingResponse{}, status.Error(codes.Unimplemented, "NOT IMEPLEMENTED")
+}
+
+func (s *Server) UnlockBalance(ctx context.Context, in *npool.UnlockBalanceRequest) (*npool.UnlockBalanceResponse, error) {
+	return &npool.UnlockBalanceResponse{}, status.Error(codes.Unimplemented, "NOT IMEPLEMENTED")
 }
--- pkg/ledger/create.go
+++ pkg/ledger/create.go
@@ -2,10 +2,23 @@ package ledger
 
 import (
 	"context"
+	"fmt"
 
 	detailmgrpb "github.com/NpoolPlatform/message/npool/ledger/mgr/v1/ledger/detail"
+
+	"github.com/shopspring/decimal"
 )
 
 func BookKeeping(ctx context.Context, in *detailmgrpb.DetailReq) error {
+	return fmt.Errorf("NOT IMPLEMENTED")
+}
 
+func UnlockBalance(
+	ctx context.Context,
+	appID, userID, coinTypeID string,
+	ioType detailmgrpb.IOType,
+	unlocked, outcoming decimal.Decimal,
+	ioExtra string,
+) error {
+	return fmt.Errorf("NOT IMPLEMENTED")
 }
