dd78d14765ee2a853eb3784bcd3a70275ae61469
fxnt <1330672764@qq.com>
2022-09-02 18:45:48 +0800

    add v2 client

---
+++ pkg/client/v2/ledger/ledger.go
@@ -0,0 +1,45 @@
+//nolint:dupl
+package ledger
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	detailpb "github.com/NpoolPlatform/message/npool/ledger/mgr/v1/ledger/detail"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger"
+
+	constant "github.com/NpoolPlatform/ledger-middleware/pkg/message/const"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func withClient(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func BookKeeping(ctx context.Context, infos []*detailpb.DetailReq) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		_, err := cli.BookKeeping(_ctx, &npool.BookKeepingRequest{
+			Infos: infos,
+		})
+		return nil, err
+	})
+	return err
+}
