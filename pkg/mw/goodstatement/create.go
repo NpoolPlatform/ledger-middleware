b62dbcd711539f56b103f3f73627c697a9e4e478
lidaqiang <dqli161@163.com>
2023-08-10 11:37:18 +0800

    add good statement

---
+++ pkg/mw/goodstatement/create.go
@@ -0,0 +1,40 @@
+package goodstatement
+
+import (
+	"context"
+	"fmt"
+
+	crud "github.com/NpoolPlatform/ledger-middleware/pkg/crud/goodstatement"
+	"github.com/NpoolPlatform/ledger-middleware/pkg/db"
+	"github.com/NpoolPlatform/ledger-middleware/pkg/db/ent"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/goodstatement"
+	"github.com/google/uuid"
+)
+
+func (h *Handler) CreateGoodStatement(ctx context.Context) (*npool.GoodStatement, error) {
+	if h.GoodID == nil {
+		return nil, fmt.Errorf("invalid good id")
+	}
+	if h.CoinTypeID == nil {
+		return nil, fmt.Errorf("invalid coin type id")
+	}
+
+	id := uuid.New()
+	if h.ID == nil {
+		h.ID = &id
+	}
+
+	if err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := crud.CreateSet(
+			cli.GoodStatement.Create(),
+			&h.Req,
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return h.GetGoodStatement(ctx)
+}
