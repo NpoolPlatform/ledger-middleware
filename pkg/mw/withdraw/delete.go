2350a2345dd18e59ef0fc10715226a269b95a570
lidaqiang <dqli161@163.com>
2023-08-10 10:51:05 +0800

    delete withdraw

---
+++ pkg/mw/withdraw/delete.go
@@ -0,0 +1,42 @@
+package withdraw
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/NpoolPlatform/ledger-middleware/pkg/db"
+	"github.com/NpoolPlatform/ledger-middleware/pkg/db/ent"
+	entwithdraw "github.com/NpoolPlatform/ledger-middleware/pkg/db/ent/withdraw"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/withdraw"
+)
+
+func (h *Handler) DeleteWithdraw(ctx context.Context) (*npool.Withdraw, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+
+	info, err := h.GetWithdraw(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		now := uint32(time.Now().Unix())
+		if _, err := cli.Withdraw.
+			Update().
+			Where(
+				entwithdraw.ID(*h.ID),
+			).
+			SetDeletedAt(now).
+			Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
