f435554e0621d420f3b4e330ea52d14307e05046
lidaqiang <dqli161@163.com>
2023-08-14 12:28:48 +0800

    add statement query

--- api/ledger/query.go
+++ api/ledger/query.go
@@ -3,34 +3,73 @@ package ledger
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	ledger1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/ledger"
-	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v1/ledger"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (s *Server) GetIntervalLedgers(ctx context.Context, in *npool.GetIntervalLedgersRequest) (
-	*npool.GetIntervalLedgersResponse,
+func (s *Server) GetLedgers(ctx context.Context, in *npool.GetLedgersRequest) (
+	*npool.GetLedgersResponse,
 	error,
 ) {
 	handler, err := ledger1.NewHandler(
 		ctx,
-		ledger1.WithAppID(&in.AppID),
-		ledger1.WithUserID(&in.UserID),
-		ledger1.WithStart(in.Start),
-		ledger1.WithEnd(in.End),
+		ledger1.WithConds(in.GetConds()),
 		ledger1.WithOffset(in.Offset),
 		ledger1.WithLimit(in.Limit),
 	)
 	if err != nil {
-		return &npool.GetIntervalLedgersResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		logger.Sugar().Errorw(
+			"GetLedgers",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetLedgersResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
+
 	infos, total, err := handler.GetLedgers(ctx)
 	if err != nil {
-		return &npool.GetIntervalLedgersResponse{}, status.Error(codes.Internal, err.Error())
+		logger.Sugar().Errorw(
+			"GetLedgers",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetLedgersResponse{}, status.Error(codes.Internal, err.Error())
 	}
-	return &npool.GetIntervalLedgersResponse{
+
+	return &npool.GetLedgersResponse{
 		Infos: infos,
 		Total: total,
+	}, nil
+}
+
+func (s *Server) GetLedger(ctx context.Context, in *npool.GetLedgerRequest) (*npool.GetLedgerResponse, error) {
+	handler, err := ledger1.NewHandler(
+		ctx,
+		ledger1.WithID(&in.ID),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetLedger",
+			"In", in,
+			"error", err,
+		)
+		return &npool.GetLedgerResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.GetLedger(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetLedger",
+			"In", in,
+			"error", err,
+		)
+		return &npool.GetLedgerResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetLedgerResponse{
+		Info: info,
 	}, nil
 }
--- api/statement/query.go
+++ api/statement/query.go
@@ -3,33 +3,43 @@ package statement
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	statement1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/statement"
 	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/statement"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (s *Server) GetIntervalStatements(ctx context.Context, in *npool.GetIntervalStatementsResponse) (
-	*npool.GetIntervalStatementsResponse,
+func (s *Server) GetStatements(ctx context.Context, in *npool.GetStatementsRequest) (
+	*npool.GetStatementsResponse,
 	error,
 ) {
 	handler, err := statement1.NewHandler(
 		ctx,
-		statement1.WithAppID(&in.AppID),
-		statement1.WithUserID(&in.UserID),
-		statement1.WithStart(in.Start),
-		statement1.WithEnd(in.End),
+		statement1.WithConds(in.GetConds()),
 		statement1.WithOffset(in.Offset),
 		statement1.WithLimit(in.Limit),
 	)
 	if err != nil {
-		return &npool.GetIntervalStatementsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		logger.Sugar().Errorw(
+			"GetStatements",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetStatementsResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
+
 	infos, total, err := handler.GetStatements(ctx)
 	if err != nil {
-		return &npool.GetIntervalStatementsResponse{}, status.Error(codes.Internal, err.Error())
+		logger.Sugar().Errorw(
+			"GetStatements",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetStatementsResponse{}, status.Error(codes.Internal, err.Error())
 	}
-	return &npool.GetIntervalStatementsResponse{
+
+	return &npool.GetStatementsResponse{
 		Infos: infos,
 		Total: total,
 	}, nil
