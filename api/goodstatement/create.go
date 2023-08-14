6a30afd1a6867dfe34d211bb531ab5981a572548
lidaqiang <dqli161@163.com>
2023-08-14 14:24:28 +0800

    add goodledger api

--- api/goodstatement/create.go
+++ api/goodstatement/create.go
@@ -1,1 +1,45 @@
 package goodstatement
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	goodstatement1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/goodstatement"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/goodstatement"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateGoodStatement(ctx context.Context, in *npool.CreateGoodStatementRequest) (*npool.CreateGoodStatementResponse, error) {
+	req := in.GetInfo()
+	handler, err := goodstatement1.NewHandler(
+		ctx,
+		goodstatement1.WithID(req.ID),
+		goodstatement1.WithGoodID(req.GoodID),
+		goodstatement1.WithCoinTypeID(req.CoinTypeID),
+		goodstatement1.WithAmount(req.Amount),
+		goodstatement1.WithBenefitDate(req.BenefitDate),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateGoodStatement",
+			"Req", in,
+			"Error", err,
+		)
+		return &npool.CreateGoodStatementResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	info, err := handler.CreateGoodStatement(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateGoodStatement",
+			"Req", in,
+			"Error", err,
+		)
+		return &npool.CreateGoodStatementResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.CreateGoodStatementResponse{
+		Info: info,
+	}, nil
+}
--- api/goodstatement/query.go
+++ api/goodstatement/query.go
@@ -1,1 +1,46 @@
 package goodstatement
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	goodstatement1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/goodstatement"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/goodstatement"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) GetGoodStatements(ctx context.Context, in *npool.GetGoodStatementsRequest) (
+	*npool.GetGoodStatementsResponse,
+	error,
+) {
+	handler, err := goodstatement1.NewHandler(
+		ctx,
+		goodstatement1.WithConds(in.GetConds()),
+		goodstatement1.WithOffset(in.Offset),
+		goodstatement1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoodStatements",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodStatementsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetGoodStatements(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoodStatements",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodStatementsResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetGoodStatementsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
