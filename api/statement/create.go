96c2595ec0c83c2628e00cc309a7943207bd8f26
lidaqiang <dqli161@163.com>
2023-08-16 17:01:24 +0800

    refactor createstatements

--- api/statement/create.go
+++ api/statement/create.go
@@ -11,80 +11,6 @@ import (
 )
 
 //nolint
-func (s *Server) CreateStatement(ctx context.Context, in *npool.CreateStatementRequest) (*npool.CreateStatementResponse, error) {
-	req := in.GetInfo()
-	handler, err := statement1.NewHandler(
-		ctx,
-		statement1.WithAppID(req.AppID),
-		statement1.WithUserID(req.UserID),
-		statement1.WithCoinTypeID(req.CoinTypeID),
-		statement1.WithIOType(req.IOType),
-		statement1.WithIOSubType(req.IOSubType),
-		statement1.WithAmount(req.Amount),
-		statement1.WithIOExtra(req.IOExtra),
-	)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateStatement",
-			"Req", in,
-			"Error", err,
-		)
-		return &npool.CreateStatementResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	info, err := handler.CreateStatement(ctx)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateStatement",
-			"Req", in,
-			"Error", err,
-		)
-		return &npool.CreateStatementResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	return &npool.CreateStatementResponse{
-		Info: info,
-	}, nil
-}
-
-//nolint
-func (s *Server) UnCreateStatement(ctx context.Context, in *npool.UnCreateStatementRequest) (*npool.UnCreateStatementResponse, error) {
-	req := in.GetInfo()
-	handler, err := statement1.NewHandler(
-		ctx,
-		statement1.WithAppID(req.AppID),
-		statement1.WithUserID(req.UserID),
-		statement1.WithCoinTypeID(req.CoinTypeID),
-		statement1.WithIOType(req.IOType),
-		statement1.WithIOSubType(req.IOSubType),
-		statement1.WithAmount(req.Amount),
-		statement1.WithIOExtra(req.IOExtra),
-	)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateStatement",
-			"Req", in,
-			"Error", err,
-		)
-		return &npool.UnCreateStatementResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	info, err := handler.CreateStatement(ctx)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateStatement",
-			"Req", in,
-			"Error", err,
-		)
-		return &npool.UnCreateStatementResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	return &npool.UnCreateStatementResponse{
-		Info: info,
-	}, nil
-}
-
-//nolint
 func (s *Server) CreateStatements(ctx context.Context, in *npool.CreateStatementsRequest) (*npool.CreateStatementsResponse, error) {
 	handler, err := statement1.NewHandler(
 		ctx,
@@ -114,6 +40,7 @@ func (s *Server) CreateStatements(ctx context.Context, in *npool.CreateStatement
 	}, nil
 }
 
+//nolint
 func (s *Server) UnCreateStatements(ctx context.Context, in *npool.UnCreateStatementsRequest) (*npool.UnCreateStatementsResponse, error) {
 	handler, err := statement1.NewHandler(
 		ctx,
@@ -121,17 +48,17 @@ func (s *Server) UnCreateStatements(ctx context.Context, in *npool.UnCreateState
 	)
 	if err != nil {
 		logger.Sugar().Errorw(
-			"CreateStatements",
+			"UnCreateStatements",
 			"Req", in,
 			"Error", err,
 		)
 		return &npool.UnCreateStatementsResponse{}, status.Error(codes.Aborted, err.Error())
 	}
 
-	infos, err := handler.CreateStatements(ctx)
+	infos, err := handler.UnCreateStatements(ctx)
 	if err != nil {
 		logger.Sugar().Errorw(
-			"CreateStatements",
+			"UnCreateStatements",
 			"Req", in,
 			"Error", err,
 		)
