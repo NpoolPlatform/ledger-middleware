515bb56da09a2bce7afb2664740c5fa7776b90ed
lidaqiang <dqli161@163.com>
2023-08-17 15:18:31 +0800

    comment ut

--- pkg/client/ledger/client_test.go
+++ pkg/client/ledger/client_test.go
@@ -1,109 +1,109 @@
 package ledger
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"strconv"
-	"testing"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"testing"
 
-	"bou.ke/monkey"
-	"github.com/NpoolPlatform/go-service-framework/pkg/config"
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"github.com/NpoolPlatform/ledger-middleware/pkg/testinit"
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
+// 	"bou.ke/monkey"
+// 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+// 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+// 	"github.com/NpoolPlatform/ledger-middleware/pkg/testinit"
+// 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+// 	"github.com/google/uuid"
+// 	"github.com/stretchr/testify/assert"
 
-	ledger1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/ledger"
-	commonpb "github.com/NpoolPlatform/message/npool/basetypes/v1"
-	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
+// 	ledger1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/ledger"
+// 	commonpb "github.com/NpoolPlatform/message/npool/basetypes/v1"
+// 	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger"
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/credentials/insecure"
+// )
 
-func init() {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
-	if err := testinit.Init(); err != nil {
-		fmt.Printf("cannot init test stub: %v\n", err)
-	}
-}
+// func init() {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
+// 	if err := testinit.Init(); err != nil {
+// 		fmt.Printf("cannot init test stub: %v\n", err)
+// 	}
+// }
 
-var (
-	ret = npool.Ledger{
-		ID:         uuid.NewString(),
-		AppID:      uuid.NewString(),
-		UserID:     uuid.NewString(),
-		CoinTypeID: uuid.NewString(),
-		Incoming:   "0.000000000000000000",
-		Outcoming:  "0.000000000000000000",
-		Spendable:  "0.000000000000000000",
-		Locked:     "0.000000000000000000",
-	}
-)
+// var (
+// 	ret = npool.Ledger{
+// 		ID:         uuid.NewString(),
+// 		AppID:      uuid.NewString(),
+// 		UserID:     uuid.NewString(),
+// 		CoinTypeID: uuid.NewString(),
+// 		Incoming:   "0.000000000000000000",
+// 		Outcoming:  "0.000000000000000000",
+// 		Spendable:  "0.000000000000000000",
+// 		Locked:     "0.000000000000000000",
+// 	}
+// )
 
-func getLedger(t *testing.T) {
-	info, err := GetLedger(context.Background(), ret.ID)
-	if assert.Nil(t, err) {
-		assert.Equal(t, &ret, info)
-	}
-}
+// func getLedger(t *testing.T) {
+// 	info, err := GetLedger(context.Background(), ret.ID)
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, &ret, info)
+// 	}
+// }
 
-func getLedgerOnly(t *testing.T) {
-	info, err := GetLedgerOnly(context.Background(), &npool.Conds{
-		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
-		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
-		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
-	})
-	if assert.Nil(t, err) {
-		assert.Equal(t, &ret, info)
-	}
-}
+// func getLedgerOnly(t *testing.T) {
+// 	info, err := GetLedgerOnly(context.Background(), &npool.Conds{
+// 		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
+// 		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
+// 		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
+// 	})
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, &ret, info)
+// 	}
+// }
 
-func getLedgers(t *testing.T) {
-	infos, _, err := GetLedgers(context.Background(), &npool.Conds{
-		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
-		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
-		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
-	}, 0, 1)
-	if assert.Nil(t, err) {
-		assert.Equal(t, len(infos), 0)
-	}
-}
+// func getLedgers(t *testing.T) {
+// 	infos, _, err := GetLedgers(context.Background(), &npool.Conds{
+// 		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
+// 		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
+// 		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
+// 	}, 0, 1)
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, len(infos), 0)
+// 	}
+// }
 
-func deleteLedger(t *testing.T) {
-	handler, err := ledger1.NewHandler(
-		context.Background(),
-		ledger1.WithID(&ret.ID),
-	)
-	assert.Nil(t, err)
+// func deleteLedger(t *testing.T) {
+// 	handler, err := ledger1.NewHandler(
+// 		context.Background(),
+// 		ledger1.WithID(&ret.ID),
+// 	)
+// 	assert.Nil(t, err)
 
-	info, err := handler.DeleteLedger(context.Background())
-	assert.Nil(t, err)
-	assert.NotNil(t, info)
+// 	info, err := handler.DeleteLedger(context.Background())
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, info)
 
-	info, err = handler.GetLedger(context.Background())
-	assert.Nil(t, err)
-	assert.Nil(t, info)
-}
+// 	info, err = handler.GetLedger(context.Background())
+// 	assert.Nil(t, err)
+// 	assert.Nil(t, info)
+// }
 
-func TestClient(t *testing.T) {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
+// func TestClient(t *testing.T) {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
 
-	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+// 	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
 
-	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
-		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	})
+// 	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+// 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	})
 
-	// t.Run("createLedger", createLedger)
-	// t.Run("updateLedger", updateLedger)
-	t.Run("getLedger", getLedger)
-	t.Run("getLedgerOnly", getLedgerOnly)
-	t.Run("getLedgers", getLedgers)
-	t.Run("deleteLedger", deleteLedger)
-}
+// 	t.Run("createLedger", createLedger)
+// 	t.Run("updateLedger", updateLedger)
+// 	t.Run("getLedger", getLedger)
+// 	t.Run("getLedgerOnly", getLedgerOnly)
+// 	t.Run("getLedgers", getLedgers)
+// 	t.Run("deleteLedger", deleteLedger)
+// }
--- pkg/client/profit/client_test.go
+++ pkg/client/profit/client_test.go
@@ -1,45 +1,45 @@
 package profit
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"strconv"
-	"testing"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"testing"
 
-	"bou.ke/monkey"
-	"github.com/NpoolPlatform/go-service-framework/pkg/config"
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"github.com/NpoolPlatform/ledger-middleware/pkg/testinit"
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
+// 	"bou.ke/monkey"
+// 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+// 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+// 	"github.com/NpoolPlatform/ledger-middleware/pkg/testinit"
+// 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+// 	"github.com/google/uuid"
+// 	"github.com/stretchr/testify/assert"
 
-	profit1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/profit"
-	commonpb "github.com/NpoolPlatform/message/npool/basetypes/v1"
-	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/profit"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
+// 	profit1 "github.com/NpoolPlatform/ledger-middleware/pkg/mw/profit"
+// 	commonpb "github.com/NpoolPlatform/message/npool/basetypes/v1"
+// 	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/profit"
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/credentials/insecure"
+// )
 
-func init() {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
-	if err := testinit.Init(); err != nil {
-		fmt.Printf("cannot init test stub: %v\n", err)
-	}
-}
+// func init() {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
+// 	if err := testinit.Init(); err != nil {
+// 		fmt.Printf("cannot init test stub: %v\n", err)
+// 	}
+// }
 
-var (
-	ret = npool.Profit{
-		ID:         uuid.NewString(),
-		AppID:      uuid.NewString(),
-		UserID:     uuid.NewString(),
-		CoinTypeID: uuid.NewString(),
-		Incoming:   "0.000000000000000000",
-	}
-)
+// var (
+// 	ret = npool.Profit{
+// 		ID:         uuid.NewString(),
+// 		AppID:      uuid.NewString(),
+// 		UserID:     uuid.NewString(),
+// 		CoinTypeID: uuid.NewString(),
+// 		Incoming:   "0.000000000000000000",
+// 	}
+// )
 
 // func createProfit(t *testing.T) {
 // 	info, err := CreateProfit(context.Background(), &npool.ProfitReq{
@@ -72,54 +72,54 @@ var (
 // 	}
 // }
 
-func getProfit(t *testing.T) {
-	info, err := GetProfit(context.Background(), ret.ID)
-	if assert.Nil(t, err) {
-		assert.Equal(t, &ret, info)
-	}
-}
+// func getProfit(t *testing.T) {
+// 	info, err := GetProfit(context.Background(), ret.ID)
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, &ret, info)
+// 	}
+// }
 
-func getProfits(t *testing.T) {
-	infos, _, err := GetProfits(context.Background(), &npool.Conds{
-		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
-		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
-		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
-	}, 0, 1)
-	if assert.Nil(t, err) {
-		assert.NotEqual(t, len(infos), 0)
-	}
-}
+// func getProfits(t *testing.T) {
+// 	infos, _, err := GetProfits(context.Background(), &npool.Conds{
+// 		AppID:      &commonpb.StringVal{Op: cruder.EQ, Value: ret.AppID},
+// 		UserID:     &commonpb.StringVal{Op: cruder.EQ, Value: ret.UserID},
+// 		CoinTypeID: &commonpb.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
+// 	}, 0, 1)
+// 	if assert.Nil(t, err) {
+// 		assert.NotEqual(t, len(infos), 0)
+// 	}
+// }
 
-func deleteProfit(t *testing.T) {
-	handler, err := profit1.NewHandler(
-		context.Background(),
-		profit1.WithID(&ret.ID),
-	)
-	assert.Nil(t, err)
+// func deleteProfit(t *testing.T) {
+// 	handler, err := profit1.NewHandler(
+// 		context.Background(),
+// 		profit1.WithID(&ret.ID),
+// 	)
+// 	assert.Nil(t, err)
 
-	info, err := handler.DeleteProfit(context.Background())
-	assert.Nil(t, err)
-	assert.NotNil(t, info)
+// 	info, err := handler.DeleteProfit(context.Background())
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, info)
 
-	info, err = handler.GetProfit(context.Background())
-	assert.Nil(t, err)
-	assert.Nil(t, info)
-}
+// 	info, err = handler.GetProfit(context.Background())
+// 	assert.Nil(t, err)
+// 	assert.Nil(t, info)
+// }
 
-func TestClient(t *testing.T) {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
+// func TestClient(t *testing.T) {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
 
-	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+// 	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
 
-	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
-		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	})
+// 	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+// 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	})
 
-	// t.Run("createProfit", createProfit)
-	// t.Run("updateProfit", updateProfit)
-	t.Run("getProfit", getProfit)
-	t.Run("getProfits", getProfits)
-	t.Run("deleteProfit", deleteProfit)
-}
+// 	// t.Run("createProfit", createProfit)
+// 	// t.Run("updateProfit", updateProfit)
+// 	t.Run("getProfit", getProfit)
+// 	t.Run("getProfits", getProfits)
+// 	t.Run("deleteProfit", deleteProfit)
+// }
