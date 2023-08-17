04342541be3e1e008fa7bac195ee98b8e0736560
lidaqiang <dqli161@163.com>
2023-08-17 18:03:51 +0800

    add client

---
+++ pkg/client/ledger/lock/client.go
@@ -0,0 +1,93 @@
+package lock
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"github.com/NpoolPlatform/ledger-middleware/pkg/servicename"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	ledgerpb "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger/lock"
+)
+
+func do(ctx context.Context, fn func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error)) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, 10*time.Second) //nolint
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return fn(_ctx, cli)
+}
+
+func LockBalance(ctx context.Context, in *npool.BalanceReq) (*ledgerpb.Ledger, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.LockBalance(ctx, &npool.LockBalanceRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail lock balance: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail lock balance: %v", err)
+	}
+	return info.(*ledgerpb.Ledger), nil
+}
+
+func UnlockBalance(ctx context.Context, in *npool.BalanceReq) (*ledgerpb.Ledger, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UnlockBalance(ctx, &npool.UnlockBalanceRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail unlock balance: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail unlock balance: %v", err)
+	}
+	return info.(*ledgerpb.Ledger), nil
+}
+
+func SpendBalance(ctx context.Context, in *npool.SpendBalanceReq) ([]*ledgerpb.Ledger, error) {
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.SpendBalance(ctx, &npool.SpendBalanceRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail spend balance: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail spend balance: %v", err)
+	}
+	return infos.([]*ledgerpb.Ledger), nil
+}
+
+func UnspendBalance(ctx context.Context, in *npool.SpendBalanceReq) ([]*ledgerpb.Ledger, error) {
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UnspendBalance(ctx, &npool.UnspendBalanceRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail unspend balance: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail unspend balance: %v", err)
+	}
+	return infos.([]*ledgerpb.Ledger), nil
+}
