797ac4282e51e0d6458d92be85a072ef08c7ba25
lidaqiang <dqli161@163.com>
2023-09-01 01:55:17 +0000

    change return msg

--- pkg/mw/ledger/statement/create.go
+++ pkg/mw/ledger/statement/create.go
@@ -176,7 +176,7 @@ func (h *createHandler) createOrUpdateLedger(ctx context.Context, tx *ent.Tx, re
 
 	if info == nil {
 		if *req.IOType != basetypes.IOType_Incoming {
-			return fmt.Errorf("spendable less than 0 %v", spendable)
+			return fmt.Errorf("spendable less than 0")
 		}
 		if _, err := ledgercrud.CreateSet(
 			tx.Ledger.Create(),
