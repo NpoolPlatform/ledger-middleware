0ff5a56ef398919b53f7aeb09efa1f2154110fc7
lidaqiang <dqli161@163.com>
2023-12-18 11:01:35 +0800

    add handler

---
+++ pkg/crud/withdraw/coupon/crud.go
@@ -0,0 +1,193 @@
+package coupon
+
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+
+	"github.com/NpoolPlatform/ledger-middleware/pkg/db/ent"
+	entcouponwithdraw "github.com/NpoolPlatform/ledger-middleware/pkg/db/ent/couponwithdraw"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
+	"github.com/google/uuid"
+)
+
+type Req struct {
+	ID         *uint32
+	EntID      *uuid.UUID
+	AppID      *uuid.UUID
+	UserID     *uuid.UUID
+	CoinTypeID *uuid.UUID
+	Amount     *decimal.Decimal
+	CouponID   *uuid.UUID
+	ReviewID   *uuid.UUID
+	State      *basetypes.WithdrawState
+	CreatedAt  *uint32
+	DeletedAt  *uint32
+}
+
+func CreateSet(c *ent.CouponWithdrawCreate, in *Req) *ent.CouponWithdrawCreate {
+	if in.ID != nil {
+		c.SetID(*in.ID)
+	}
+	if in.EntID != nil {
+		c.SetEntID(*in.EntID)
+	}
+	if in.AppID != nil {
+		c.SetAppID(*in.AppID)
+	}
+	if in.UserID != nil {
+		c.SetUserID(*in.UserID)
+	}
+	if in.CoinTypeID != nil {
+		c.SetCoinTypeID(*in.CoinTypeID)
+	}
+	if in.CouponID != nil {
+		c.SetCouponID(*in.CouponID)
+	}
+	if in.ReviewID != nil {
+		c.SetReviewID(*in.ReviewID)
+	}
+	if in.Amount != nil {
+		c.SetAmount(*in.Amount)
+	}
+	c.SetState(basetypes.WithdrawState_Reviewing.String())
+	return c
+}
+
+func UpdateSet(u *ent.CouponWithdrawUpdateOne, req *Req) *ent.CouponWithdrawUpdateOne {
+	if req.ReviewID != nil {
+		u.SetReviewID(*req.ReviewID)
+	}
+	if req.State != nil {
+		u.SetState(req.State.String())
+	}
+	if req.DeletedAt != nil {
+		u.SetDeletedAt(*req.DeletedAt)
+	}
+	return u
+}
+
+type Conds struct {
+	EntID      *cruder.Cond
+	AppID      *cruder.Cond
+	UserID     *cruder.Cond
+	CoinTypeID *cruder.Cond
+	State      *cruder.Cond
+	Amount     *cruder.Cond
+	ReviewID   *cruder.Cond
+	CouponID   *cruder.Cond
+	CreatedAt  *cruder.Cond
+}
+
+func SetQueryConds(q *ent.CouponWithdrawQuery, conds *Conds) (*ent.CouponWithdrawQuery, error) { //nolint
+	q.Where(entcouponwithdraw.DeletedAt(0))
+	if conds == nil {
+		return q, nil
+	}
+	if conds.AppID != nil {
+		appID, ok := conds.AppID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid appid")
+		}
+		switch conds.AppID.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.AppID(appID))
+		default:
+			return nil, fmt.Errorf("invalid appid op field")
+		}
+	}
+	if conds.UserID != nil {
+		userID, ok := conds.UserID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid userid")
+		}
+		switch conds.UserID.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.UserID(userID))
+		default:
+			return nil, fmt.Errorf("invalid userid op field")
+		}
+	}
+	if conds.CoinTypeID != nil {
+		coinTypeID, ok := conds.CoinTypeID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid cointypeid")
+		}
+		switch conds.CoinTypeID.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.CoinTypeID(coinTypeID))
+		default:
+			return nil, fmt.Errorf("invalid cointypeid op field")
+		}
+	}
+	if conds.Amount != nil {
+		Amount, ok := conds.Amount.Val.(decimal.Decimal)
+		if !ok {
+			return nil, fmt.Errorf("invalid amount")
+		}
+		switch conds.Amount.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.Amount(Amount))
+		default:
+			return nil, fmt.Errorf("invalid amount op field")
+		}
+	}
+	if conds.State != nil {
+		state, ok := conds.State.Val.(basetypes.WithdrawState)
+		if !ok {
+			return nil, fmt.Errorf("invalid state")
+		}
+		switch conds.State.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.State(state.String()))
+		default:
+			return nil, fmt.Errorf("invalid state op field")
+		}
+	}
+	if conds.CreatedAt != nil {
+		createdAt, ok := conds.CreatedAt.Val.(uint32)
+		if !ok {
+			return nil, fmt.Errorf("invalid created at")
+		}
+		switch conds.CreatedAt.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.CreatedAt(createdAt))
+		case cruder.GT:
+			q.Where(entcouponwithdraw.CreatedAtGT(createdAt))
+		case cruder.GTE:
+			q.Where(entcouponwithdraw.CreatedAtGTE(createdAt))
+		case cruder.LT:
+			q.Where(entcouponwithdraw.CreatedAtLT(createdAt))
+		case cruder.LTE:
+			q.Where(entcouponwithdraw.CreatedAtLTE(createdAt))
+		default:
+			return nil, fmt.Errorf("invalid createdat op field")
+		}
+	}
+	if conds.ReviewID != nil {
+		reviewID, ok := conds.ReviewID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid reviewid")
+		}
+		switch conds.ReviewID.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.ReviewID(reviewID))
+		default:
+			return nil, fmt.Errorf("invalid reviewid op field")
+		}
+	}
+	if conds.CouponID != nil {
+		couponID, ok := conds.CouponID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid couponid")
+		}
+		switch conds.CouponID.Op {
+		case cruder.EQ:
+			q.Where(entcouponwithdraw.CouponID(couponID))
+		default:
+			return nil, fmt.Errorf("invalid couponid op field")
+		}
+	}
+	return q, nil
+}
---
+++ pkg/mw/withdraw/coupon/handler.go
@@ -0,0 +1,256 @@
+package coupon
+
+import (
+	"context"
+	"fmt"
+
+	constant "github.com/NpoolPlatform/ledger-middleware/pkg/const"
+	crud "github.com/NpoolPlatform/ledger-middleware/pkg/crud/withdraw/coupon"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
+	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/withdraw/coupon"
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+type Handler struct {
+	crud.Req
+	Conds  *crud.Conds
+	Offset int32
+	Limit  int32
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(id *uint32, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid id")
+			}
+			return nil
+		}
+		h.ID = id
+		return nil
+	}
+}
+
+func WithEntID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid entid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.EntID = &_id
+		return nil
+	}
+}
+
+func WithAppID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid app id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.AppID = &_id
+		return nil
+	}
+}
+
+func WithUserID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid user id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.UserID = &_id
+		return nil
+	}
+}
+
+func WithCoinTypeID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid coin type id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.CoinTypeID = &_id
+		return nil
+	}
+}
+
+func WithCouponID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid coupon id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.CouponID = &_id
+		return nil
+	}
+}
+
+func WithAmount(amount *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if amount == nil {
+			if must {
+				return fmt.Errorf("invalid amount")
+			}
+			return nil
+		}
+		_amount, err := decimal.NewFromString(*amount)
+		if err != nil {
+			return err
+		}
+		if _amount.Cmp(decimal.NewFromInt(0)) < 0 {
+			return fmt.Errorf("amount is less than 0 %v", *amount)
+		}
+		h.Amount = &_amount
+		return nil
+	}
+}
+
+//nolint
+func WithState(state *basetypes.WithdrawState, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if state == nil {
+			if must {
+				return fmt.Errorf("invalid state")
+			}
+			return nil
+		}
+		switch *state {
+		case basetypes.WithdrawState_Reviewing:
+		case basetypes.WithdrawState_Approved:
+		case basetypes.WithdrawState_Rejected:
+		default:
+			return fmt.Errorf("invalid state")
+		}
+		h.State = state
+		return nil
+	}
+}
+
+func WithReviewID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid review id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.ReviewID = &_id
+		return nil
+	}
+}
+
+func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Conds = &crud.Conds{}
+		if conds == nil {
+			return nil
+		}
+		if conds.AppID != nil {
+			id, err := uuid.Parse(conds.GetAppID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.AppID = &cruder.Cond{Op: conds.GetAppID().GetOp(), Val: id}
+		}
+		if conds.UserID != nil {
+			id, err := uuid.Parse(conds.GetUserID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.UserID = &cruder.Cond{Op: conds.GetUserID().GetOp(), Val: id}
+		}
+		if conds.CoinTypeID != nil {
+			id, err := uuid.Parse(conds.GetCoinTypeID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.CoinTypeID = &cruder.Cond{Op: conds.GetCoinTypeID().GetOp(), Val: id}
+		}
+		if conds.ReviewID != nil {
+			id, err := uuid.Parse(conds.GetReviewID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.ReviewID = &cruder.Cond{Op: conds.GetReviewID().GetOp(), Val: id}
+		}
+		if conds.CouponID != nil {
+			id, err := uuid.Parse(conds.GetCouponID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.CouponID = &cruder.Cond{Op: conds.GetCouponID().GetOp(), Val: id}
+		}
+		if conds.State != nil {
+			state := conds.GetState().GetValue()
+			h.Conds.State = &cruder.Cond{Op: conds.GetState().GetOp(), Val: basetypes.WithdrawState(state)}
+		}
+		if conds.CreatedAt != nil {
+			h.Conds.CreatedAt = &cruder.Cond{Op: conds.GetCreatedAt().GetOp(), Val: conds.GetCreatedAt().GetValue()}
+		}
+		return nil
+	}
+}
+
+func WithOffset(offset int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = offset
+		return nil
+	}
+}
+
+func WithLimit(limit int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if limit == 0 {
+			limit = constant.DefaultRowLimit
+		}
+		h.Limit = limit
+		return nil
+	}
+}
