53014e106c936dddc508ca959e9cceb35af50fab
lidaqiang <dqli161@163.com>
2023-12-20 17:18:11 +0800

    check couponwithdraw when create

--- pkg/mw/withdraw/coupon/create.go
+++ pkg/mw/withdraw/coupon/create.go
@@ -2,10 +2,13 @@ package coupon
 
 import (
 	"context"
+	"fmt"
 
 	crud "github.com/NpoolPlatform/ledger-middleware/pkg/crud/withdraw/coupon"
 	"github.com/NpoolPlatform/ledger-middleware/pkg/db"
 	"github.com/NpoolPlatform/ledger-middleware/pkg/db/ent"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	ledgertypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
 	npool "github.com/NpoolPlatform/message/npool/ledger/mw/v2/withdraw/coupon"
 	"github.com/google/uuid"
 )
@@ -25,6 +28,20 @@ func (h *createHandler) createCouponWithdraw(ctx context.Context, tx *ent.Tx) er
 }
 
 func (h *Handler) CreateCouponWithdraw(ctx context.Context) (*npool.CouponWithdraw, error) {
+	h.Conds = &crud.Conds{
+		AppID:    &cruder.Cond{Op: cruder.EQ, Val: *h.AppID},
+		UserID:   &cruder.Cond{Op: cruder.EQ, Val: *h.UserID},
+		CouponID: &cruder.Cond{Op: cruder.EQ, Val: *h.CouponID},
+		State:    &cruder.Cond{Op: cruder.EQ, Val: ledgertypes.WithdrawState_Reviewing},
+	}
+	exist, err := h.ExistCouponWithdrawConds(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if exist {
+		return nil, fmt.Errorf("coupon withdraw already exist")
+	}
+
 	id := uuid.New()
 	if h.EntID == nil {
 		h.EntID = &id
@@ -34,7 +51,7 @@ func (h *Handler) CreateCouponWithdraw(ctx context.Context) (*npool.CouponWithdr
 		Handler: h,
 	}
 
-	err := db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error {
+	err = db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error {
 		if err := handler.createCouponWithdraw(ctx, tx); err != nil {
 			return err
 		}
