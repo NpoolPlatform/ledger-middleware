b159b8f74b655e3301c0827ed022b690bbc1db12
Zhao KK <kikakkz@hotmail.com>
2024-07-03 11:56:46 +0000

    add debug log

--- pkg/mw/ledger/settle.go
+++ pkg/mw/ledger/settle.go
@@ -172,7 +172,7 @@ func (h *Handler) SettleBalances(ctx context.Context) ([]*ledgermwpb.Ledger, err
 		return nil, wlog.Errorf("mismatched statementids")
 	}
 
-	err := db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error {
+	err := db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error { //nolint:dupl
 		if err := handler.lop.getLedgers(ctx, tx); err != nil {
 			return wlog.WrapError(err)
 		}
